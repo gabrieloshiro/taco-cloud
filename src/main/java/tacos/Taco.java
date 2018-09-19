77cc7f3b6be24bd97318c7dfb0a8c4e181321f59
Gabriel Oshiro <gabrieloshiro@gmail.com>
2018-09-19 00:26:43 -0400

    WIP

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -2,13 +2,22 @@ package tacos;
 
 import java.util.List;
 
+import javax.validation.constraints.Size;
+
+import org.springframework.lang.NonNull;
+
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class Taco {
-	
+
+	@NonNull
+	@Size(min = 5, message = "Name must be at least 5 characters long")
+	private String name;
+
+	@Size(min = 1, message = "You must choose at least 1 ingredient")
 	private List<String> ingredients;
 
-	private String name;
-	
 }
--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -3,15 +3,14 @@ package tacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import tacos.Ingredient.Type;
-
 @SpringBootApplication
 public class TacoCloudApplication {
 
-	public static void main(String[] args) {
-		
-		//Ingredient ingredient = new Ingredient("ABC123 ID", "taco shell", Type.CHEESE);
-		
+	public static void main(final String[] args) {
+
+		// Ingredient ingredient = new Ingredient("ABC123 ID", "taco shell",
+		// Type.CHEESE);
+
 		SpringApplication.run(TacoCloudApplication.class, args);
 	}
 }
